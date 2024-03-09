3e41f065ecfa9bd497bf09de9b986ddfc062a2eb
DShamak <dmitry.shamak.dev@gmail.com>
2024-03-09 22:38:20 +0400

    Add welcom page

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
+import fs from "fs";
 
 import services from "./services/";
 
@@ -16,12 +18,24 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
+
 const _list = Object.values(services);
 for (const service of _list) {
   service().then((router) => {
     app.use("/api", router);
   });
 }
+
+const rootFile = path.join(__dirname, "../public/index.html");
+app.use("*", (req, res) => {
+  console.log(rootFile);
+
+  if (fs.existsSync(rootFile)) {
+    res.status(200).sendFile(rootFile);
+  } else {
+    res.status(404).end("Are you lost?");
+  }
+});
 
 //exporting app
 export default app;
